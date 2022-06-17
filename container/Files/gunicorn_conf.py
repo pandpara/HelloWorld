52c1268d0488e42f8ecb513fc3a27b5ff681b248
Parag Panda <pandpara@amazon.com>
2022-06-16 22:21:49 -0700

    Add logging
    No need to install gthread

--- container/Files/gunicorn_conf.py
+++ container/Files/gunicorn_conf.py
@@ -1,7 +1,29 @@
 import os
 import threading
 from codeguru_profiler_agent import Profiler
+from logging.config import dictConfig
 
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] [%(process)d] [%(thread)d %(threadName)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    },
+    'loggers': {
+        'codeguru_profiler_agent': {
+            'level': 'INFO',
+            'handlers': ['wsgi']
+        }
+    }
+})
 
 def post_fork(server, worker):
     """
